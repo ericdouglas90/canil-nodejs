04e59de9e75296c8611375979e50062037a2383c
ensilva <ensilva@pdmfc.com>
2024-10-29 23:08:08 -0300

    create search and page not found.

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,20 @@
 import { Request, Response } from 'express';
 
+import { Pet } from '../models/pets';
+import { createMenuObject } from '../helpers/createMenuObject'; 
+
 export const search = (req: Request, res: Response) => {
+    const query: string = req.query.q as string;
 
+    if(!query) {
+        return res.redirect('/' );
+    }
 
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    })
 }
